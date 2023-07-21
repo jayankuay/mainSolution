c3638fcfa4444623ecbb94400b078938704f3adf
jayankuay <jayankuay@gmail.com>
2023-07-21 17:41:36 +0800

    I managed to somewhat complete the overheads code

--- overheads.py
+++ overheads.py
@@ -2,40 +2,66 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import csv
 
-fp = Path.cwd/"Overheads.csv"
-with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-    reader = csv.reader(file)
-    next(reader)
 
-    salary_expense = []
-    interest_expense = []
-    marketing_expense = []
-    rental_expense = []
-    overflow_expense = []
-    penalty_expense = []
-    depreciation_expense = []
-    maintenance_expense = []
-    shipping_expense = []
-    human_resource_expense = []
+def highest_overhead_expense(file):
+    """
+    Will find the highest overhead expense incurred over 90 day period of running the business
+    """
 
-    for row in reader:
-        if row[1] == "Salary Expense":
-            salary_expense.append(row[2])
-        elif row[1] == "Interest Expense":
-            interest_expense.append(row[2])
-        elif row[1] == "Marketing Expense":
-            marketing_expense.append(row[2])
-        elif row[1] == "Rental Expense":
-            rental_expense.append(row[2])
-        elif row[1] == "Overflow Expense":
-            overflow_expense.append(row[2])
-        elif row[1] == "Penalty Expense":
-            penalty_expense.append(row[2])
-        elif row[1] == "Depreciation Expense":
-            depreciation_expense.append(row[2])
-        elif row[1] == "Maintenance Expense":
-            maintenance_expense.append(row[2])
-        elif row[1] == "Shipping Expense":
-            shipping_expense.append(row[2])
-        elif row[1] == "Human Resource Expense":
-            human_resource_expense.append(row[2])
+    highest_expense = 0
+    highest_overhead = 0
+    
+    fp = Path.cwd()/"Overheads.csv"
+    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            overhead = float(row[1])
+            if overhead > highest_overhead:
+                highest_overhead = overhead
+                highest_expense = row[0]
+
+    return [highest_expense, highest_overhead]
+
+highest_overhead_report = highest_overhead_expense('Overheads.csv')   
+
+file_path = Path.cwd()/"summary_report.txt"
+file_path.touch()
+
+with file_path.open(mode = "w", encoding = "UTF-8") as file:
+    file.write(f"[HIGHEST OVERHEAD] {highest_overhead_report[0]}: {highest_overhead_report[1]}%")
+
+
+#    salary_expense = []
+#    interest_expense = []
+#    marketing_expense = []
+#    rental_expense = []
+#    overflow_expense = []
+#    penalty_expense = []
+#    depreciation_expense = []
+#    maintenance_expense = []
+#    shipping_expense = []
+#    human_resource_expense = []
+
+#    for row in reader:
+#        if row[1] == "Salary Expense":
+#           salary_expense.append(row[2])
+#        elif row[1] == "Interest Expense":
+#            interest_expense.append(row[2])
+#        elif row[1] == "Marketing Expense":
+#            marketing_expense.append(row[2])
+#        elif row[1] == "Rental Expense":
+#            rental_expense.append(row[2])
+#        elif row[1] == "Overflow Expense":
+#            overflow_expense.append(row[2])
+#        elif row[1] == "Penalty Expense":
+#            penalty_expense.append(row[2])
+#        elif row[1] == "Depreciation Expense":
+#            depreciation_expense.append(row[2])
+#        elif row[1] == "Maintenance Expense":
+#            maintenance_expense.append(row[2])
+#        elif row[1] == "Shipping Expense":
+#            shipping_expense.append(row[2])
+#        elif row[1] == "Human Resource Expense":
+#            human_resource_expense.append(row[2])
+
