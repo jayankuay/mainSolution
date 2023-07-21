6621c2a3a1d36ffd2a3ab32cf485439039409115
felleo25 <felicialeong8920@gmail.com>
2023-07-21 14:40:58 +0800

    first commit

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,3 +1,7 @@
+import csv 
+
+
+
 def total_difference():
     """
     - To calculate the difference in cash on hand 
@@ -5,7 +9,7 @@ def total_difference():
     cash_on_hand = 5408972,4922293,5284056,6335197,6011448,7061570,8469700,9192699,8290871,9130754,9641186
     for day in range(day,cash_on_hand):
         total_difference += float(cash_on_hand[day]-cash_on_hand[day-1])
-
+    return[day,cash_on_hand]
 print(total_difference)
 
 
