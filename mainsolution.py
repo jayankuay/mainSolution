f3061e2a31a01f17e58da66ea0daacf2d0dce159
jayankuay <jayankuay@gmail.com>
2023-07-28 16:46:54 +0800

    I tried modularising the files in main solution

--- mainsolution.py
+++ mainsolution.py
@@ -1,6 +1,9 @@
-def add():
-    print("This is function add")
-    return 
-def subtract():
-    print("This is function subtract")
-    return
+import cash_on_hand, overheads, profit_loss
+
+def main():
+    cash_on_hand.cashonhand_function()
+    overheads.overhead_function()
+    profit_loss.profitloss_function()
+
+main()
+
