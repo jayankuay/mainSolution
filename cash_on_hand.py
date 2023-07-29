c8a7b0d114bf8d76fc9bcaf4dd4dd1eca1988df5
jayankuay <jayankuay@gmail.com>
2023-07-29 14:22:09 +0800

    I tried but not fully correct yet

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -2,30 +2,43 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import csv
 
-def cash_difference():
+def cash_difference(file):
     """
     - TO calculate the difference in cash on hand
     """
     total_difference = 0
-    difference = 0
-    total_difference_list = 0
-    fp = Path.cwd()/"cash_on_hand.py"
+    #difference = 0
+    #total_difference_list = 0
+    cash_on_hand = 0
+    previous_cash_on_hand = 0
+    surplus_day = 0
+    highest_surplus = 0
+    fp = Path.cwd()/"Cash on Hand.csv"
     with fp.open(mode="r",encoding="UTF-8",newline="") as file:
         reader = csv.reader(file)
         next(reader)
         for row in reader:
-            if difference[1] < difference[row-1]:
-                total_difference = difference[row-1] - difference[1]
-                total_difference_list.append(total_difference)
-        return[total_difference_list]
+            cash_on_hand = float(row[1])
+            previous_cash_on_hand = 0
+            #total_difference = previous_cash_on_hand - following_cash_on_hand
+            if previous_cash_on_hand > 0:
+            #if cash_on_hand > total_difference:
+                total_difference = cash_on_hand - previous_cash_on_hand
+                if total_difference > highest_surplus:
+                    highest_surplus = total_difference
+                #total_difference_list.append(total_difference)
+                    surplus_day = row[0]
+        #return [surplus_day, highest_cash_on_hand]
+
+        return [surplus_day, total_difference]
     
-highest_difference_report = cash_difference['cash_on_hand.py']
+highest_difference_report = cash_difference('cash_on_hand.py')
 
 file_path = Path.cwd()/"summary_report.txt"
 file_path.touch()
 
 with file_path.open(mode = "w", encoding = "UTF-8") as file:
-    file.write(f"[Highest increment]=$ {highest_difference_report[1]}")
+    file.write(f"[HIGHEST CASH SURPLUS] DAY: {highest_difference_report[0]}, AMOUNT {highest_difference_report[1]}")
 
 
     
