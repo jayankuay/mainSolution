5636d816a31824b7d4f4e17ad8aa6c8ed50ad9fd
felleo25 <felicialeong8920@gmail.com>
2023-08-15 16:31:54 +0800

    deleted some parts

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -29,18 +29,7 @@ def cashonhand_function():
         cash_deficit = 0
         cash_deficit_day = 0
         cash_deficits_list = []
-                #current_cash_on_hand = float(row[1])
-                #if previous_cash_on_hand > 0 and current_cash_on_hand > previous_cash_on_hand:
-                #    total_difference = current_cash_on_hand - previous_cash_on_hand
-                #previous_cash_on_hand = current_cash_on_hand
-                #if total_difference > highest_surplus:
-                #    highest_surplus =  total_difference
-                #    highest_surplus_day = row[0]
-                #current_cash_on_hand = float(row[1])
-                #if previous_cash_on_hand > 0 and current_cash_on_hand < previous_cash_on_hand:
-                #    cash_deficit = previous_cash_on_hand - current_cash_on_hand
-                #    cash_deficit_day = row[0]
-                #previous_cash_on_hand = current_cash_on_hand  
+            
         for value in cash_on_hand: # Use for loop to loop through all the values for cash on hand
             current_cash_on_hand = float(value[1]) # Use float to convert current day cash on hand
                                                    # from string
@@ -98,36 +87,3 @@ def cashonhand_function():
   
     
 
-
-#def total_difference():
-#    """
-#    - To calculate the difference in cash on hand 
-#    """
-#    difference = 0
-#    current_day = 0
-#    next_day = 0
-#    fp = Path.cwd()/"cash_on_hand.py"
-#    with fp.open(mode="r",encoding="UTF-8",newline="") as file:
-#        reader = csv.reader(file)
-#        next(reader)
-#        for row in reader:
-#            difference += float(current_day[1]- next_day[1])
-
-
-
-    
-
-
-
-
-
-#    day = 1,2,3,4,5,6,7,8,9,10,11
-#    cash_on_hand = 5408972,4922293,5284056,6335197,6011448,7061570,8469700,9192699,8290871,9130754,9641186
-#    for day in range(day,cash_on_hand):
-#        total_difference += float(cash_on_hand[0]-cash_on_hand[1])
-#   return[day,cash_on_hand]
-#print(total_difference)
-#for highest_surplus_day, highest_surplus in highest_difference_report:
-        #if highest_surplus > 0:
-            #file.write(f"[HIGHEST CASH SURPLUS] DAY: {highest_difference_report[0]}, AMOUNT: {highest_difference_report[1]}\n")
-        #if highest_difference_report[1] < 0:
