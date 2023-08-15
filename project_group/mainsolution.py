6ecc6eda164872a90ff55dda1cf999184936fa55
jayankuay <jayankuay@gmail.com>
2023-08-15 15:09:16 +0800

    documentation

--- project_group/mainsolution.py
+++ project_group/mainsolution.py
@@ -1,6 +1,9 @@
-import overheads, cash_on_hand, profit_loss
+# mainsolution.py acts as the coordinating program which executes each python file that has been 
+# organised into functions
+import overheads, cash_on_hand, profit_loss # import python files as modules
 
-def main():
+def main():                             # functions for each module exectued in the mainsolution.py
+                                        # program
     overheads.overhead_function()
     cash_on_hand.cashonhand_function()
     profit_loss.profitloss_function()
